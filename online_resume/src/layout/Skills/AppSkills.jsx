39d74810ded81eeee08d9eaec7a89fc2ea323fe2
Decnox <loick.ca@hotmail.fr>
2025-03-07 11:11:16 +0100

    add competences layout and component

---
+++ online_resume/src/layout/Skills/AppSkills.jsx
@@ -0,0 +1,19 @@
+import { skills } from "../../utils/skills.js"
+import AppSkillsCard from "../../components/appSkillsCard.jsx"
+
+const AppSkills = () => {
+    return(
+        <div className="skills">
+            <h1>Compétences</h1>
+                <div className="skill">
+                {   
+                    skills.map((skill) => 
+                        <AppSkillsCard {...skill} />)
+                }
+                </div>
+        </div>
+        
+    )
+}
+
+export default AppSkills;
---
+++ online_resume/src/utils/skills.js
@@ -0,0 +1,50 @@
+export const skills = [
+    {
+        "name": "JavaScript",
+        "color": "#003174"
+    },
+    {
+        "name": "CSS",
+        "color": "#400b00"
+    },
+    {
+        "name": "HTML",
+        "color": "#184900"
+    },
+    {
+        "name": "PHP",
+        "color": "#003174"
+    },
+    {
+        "name": "React",
+        "color": "#400b00"
+    },
+    {
+        "name": "Python",
+        "color": "#184900"
+    },
+    {
+        "name": "Blender",
+        "color": "#003174"
+    },
+    {
+        "name": "Photoshop",
+        "color": "#400b00"
+    },
+    {
+        "name": "Maya",
+        "color": "#184900"
+    },
+    {
+        "name": "C++",
+        "color": "#003174"
+    },
+    {
+        "name": "Unity",
+        "color": "#400b00"
+    },
+    {
+        "name": "Unreal Engine",
+        "color": "#184900"
+    },
+]
