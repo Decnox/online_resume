143d3491b3c31c2dd2e3646e78865760d171d521
Chaser <florian.isak@icloud.com>
2025-05-25 16:41:36 +0200

    Re-init project properly

--- src/components/hero/hero.jsx
+++
@@ -1,8 +0,0 @@
-export default function Hero () {
-    return (
-        <div class="m-auto text-white">
-            <h1 class="lg:text-2xl font-serif md:text-2xl">Caillon Loïck</h1>
-            <h1 class="lg:text-6xl font-serif md:text-6xl">Web Developper</h1>
-        </div>
-    )
-}
